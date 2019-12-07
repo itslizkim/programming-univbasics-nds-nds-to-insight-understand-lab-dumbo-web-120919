51fe3539fd9f91785f708546800775dcb84a1fe7
Elizabeth Kim <elizabeth.kim93@gmail.com>
2019-12-07 20:56:16 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,11 +8,11 @@ end
 
 def print_first_directors_movie_titles
   ss_movies = directors_database[0][:movies]
-index = 0
+  index = 0
 
-while index < ss_movies.length do
-  titles = ss_movies[index][:title]
-  puts titles
-  index +=1
-end
+  while index < ss_movies.length do
+    titles = ss_movies[index][:title]
+    puts titles
+    index +=1
+  end
 end
