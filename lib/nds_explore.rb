77d1c85cfea6608effe0d2adc49190980fca0371
Elizabeth Kim <elizabeth.kim93@gmail.com>
2019-12-07 20:48:38 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -5,14 +5,15 @@ p 'directors_database'# Call the method di rectors_database to retrieve the NDS
 def pretty_print_nds(nds)
   require 'pp'
   pp 'directors_database'# Change the code below to pretty print the nds with pp
-  
+
 end
 
 def print_first_directors_movie_titles
-row_index = 0
-while row_index < directors_database.length do
-  column_index = 0
-  while column_index <directors_database[row_index].length do
-    if directors_database[row_index][column_index][0] == "Spielberg"
+spielberg = directors_database[0][:movies]
+i = 0
+while i < directors_database.length do
+  titles = spielberg[i][:title]
+  puts titles
+  i += 1
 end
 end
