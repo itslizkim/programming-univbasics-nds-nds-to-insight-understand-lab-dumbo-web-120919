1f00918076f29611c04174f04fab387f18895343
Elizabeth Kim <elizabeth.kim93@gmail.com>
2019-12-07 20:55:32 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,7 +4,6 @@ require 'pp'# Call the method di rectors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
   pp nds# Change the code below to pretty print the nds with pp
-  nil
 end
 
 def print_first_directors_movie_titles
