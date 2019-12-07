1108bcd51b7c8a05f5b5c22e00fe78ef9146f986
Elizabeth Kim <elizabeth.kim93@gmail.com>
2019-12-07 20:50:00 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,7 +12,7 @@ def print_first_directors_movie_titles
 spielberg = directors_database[0][:movies]
 i = 0
 while i < directors_database.length do
-  titles = spielberg[i][:title]
+  titles = spielberg[i][:titles]
   puts titles
   i += 1
 end
